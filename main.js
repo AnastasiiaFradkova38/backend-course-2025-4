6463da8f8e2e0ae924fa98ef7bfb51ed8ec4b3a1
Anastasiia bc2025-4 <anastasiia.fradkova@gmail.com>
2026-03-22 11:53:39 +0200

    Set up HTTP server to handle incoming requests

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const {program, CommanderError} = require("commander");
+const { XMLBuilder } = require("fast-xml-parser");
 const fs = require("fs");
 const http = require("http");
 
@@ -17,10 +18,48 @@ async function main() {
             throw new Error("File not found.");
         }
 
-        const server = http.createServer((request, response) => {
-            response.statusCode = 200;
-            response.setHeader("Content-Type", "text/plain");
-            response.end("Hello world!");
+        const server = http.createServer(async (request, response) => {
+            try {
+                const data = JSON.parse(await fs.promises.readFile(options["input"]));
+                const url = new URL(request.url, `http://${options["host"]}:${options["port"]}`);
+
+                let filteredData = data;
+                const minRainfall = url.searchParams.get("min_rainfall");
+                const humidity = url.searchParams.get("humidity");
+
+                if (minRainfall) {
+                    filteredData = filteredData.filter(item => item.Rainfall > parseFloat(minRainfall));
+                }
+
+                const records = filteredData.map(item => {
+                    const record = {
+                        rainfall: item.Rainfall,
+                        pressure3pm: item.Pressure3pm
+                    };
+
+                    if (humidity === "true") {
+                        record.humidity = item.Humidity3pm;
+                    }
+
+                    return record;
+                });
+
+                const builder = new XMLBuilder({format: true});
+                const xml = builder.build({
+                    weather_data: {
+                        record: records
+                    }
+                });
+
+                response.statusCode = 200;
+                response.setHeader("Content-Type", "application/xml");
+                response.end(xml);
+
+            } catch (error) {
+                console.error(error.message);
+                response.statusCode = 500;
+                response.end();
+            }
         });
 
         server.listen(options["port"], options["host"], () => console.log(`Server is listening on http://${options["host"]}:${options["port"]}.`));
